9d64ea75a9ed61d10c5cea0c6833bb3a8352ae4a
las6 <las6@mail.ru>
2017-07-23 20:59:54 +0300

    delete useless code

--- src/main/java/botoss/Consumer.java
+++ src/main/java/botoss/Consumer.java
@@ -12,35 +12,11 @@ public class Consumer {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "192.168.1.64:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("group.id", "rate-module");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        final KafkaConsumer<String, String> consumer2 = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList("to-module"));
-        consumer2.subscribe(Arrays.asList("to-connector", "test"));
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    ConsumerRecords<String, String> records = consumer2.poll(100);
-                    for (ConsumerRecord<String, String> record : records) {
-
-                        System.out.printf("topic = %s, offset = %d, key = %s, value = %s%n", record.topic(), record.offset(), record.key(), record.value());
-                    }
-                }
-            }
-        }).start();
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
@@ -48,9 +24,7 @@ public class Consumer {
                 if (((new JSONObject(record.value())).getString("command")).equals("курс")) {
                     MyProducer.rate(record.key(), (new JSONObject(record.value())).getString("connector-id"));
                 }
-                System.out.printf("topic = %s, offset = %d, key = %s, value = %s%n", record.topic(), record.offset(), record.key(), record.value());
             }
         }
-
     }
 }
--- src/main/java/botoss/MyProducer.java
+++ src/main/java/botoss/MyProducer.java
@@ -6,9 +6,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,14 +19,9 @@ public class MyProducer {
     public static void rate(String key, String connectorName) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "192.168.1.64:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        String s = null;
+        String s = "";
         try {
             s = getUrl("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+" +
                     "=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
@@ -41,7 +36,7 @@ public class MyProducer {
         }
         org.apache.kafka.clients.producer.Producer<String, String> producer = new KafkaProducer<>(props);
         JSONObject ans = new JSONObject().put("connector-id", connectorName).put("text", text);
-        producer.send(new ProducerRecord<String, String>("to-connector",  key, ans.toString()));
+        producer.send(new ProducerRecord<>("to-connector",  key, ans.toString()));
 
         producer.close();
     }
